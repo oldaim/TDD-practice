1f371bb633dfa2cda63b536fd7331260a4c025a6
oldaim <dgk0911@gmail.com>
2021-08-05 21:59:40 +0900

    #4 - 컨트롤러 수정 (read 삭제 및 코딩 스타일 일치)

--- src/main/java/com/example/tdd_project/controller/BoardController.java
+++ src/main/java/com/example/tdd_project/controller/BoardController.java
@@ -20,36 +20,32 @@ public class BoardController {
     private final BoardService boardService;
 
 
-    @GetMapping(value = "/read") //단순히 조회 기능만 넣어봄
-    public ResponseEntity<Optional<Board>> read()
-    {
 
-      return ResponseEntity.ok(boardService.read(2L));
-
-    }
-
+    //조회
     @GetMapping(value = "/read/{bno}") // URL에 변수를 사용해서 원하는 글만 조회 하게 했음
 
-    public ResponseEntity<Optional<Board>> read2(@PathVariable("bno") Long number)
+    public ResponseEntity<Optional<Board>> read(@PathVariable("bno") Long number)
     {
 
         return ResponseEntity.ok(boardService.read(number));
 
     }
 
-
+    // 저장
     @PostMapping(value = "/save") //이제 POST 요청에서 JSON 파일을 받아서 저장소에 save 할 수 있음
     public void save(@RequestBody BoardDTO dto)
     {
+
         boardService.save(dto);
     }
 
     @PostMapping(value = "/saveAll") //만약 save할 JSON 파일이 배열 형태로 오게 된다면 , 이 방식으로 저장하도록 설정
-    public void save(@RequestBody List<BoardDTO> dto)
+    public void saveAll(@RequestBody List<BoardDTO> dto)
     {
         boardService.saveAll((ArrayList<BoardDTO>) dto);
     }
 
+    //수정 , 삭제
     @PostMapping(value = "/modify") //이제 POST 요청에서 JSON 파일을 받아서 저장소에 modify 할 수 있음
     public void modify(@RequestBody BoardDTO dto)
     {
@@ -59,10 +55,7 @@ public class BoardController {
     @GetMapping(value = "/delete/{id}") // id 받아서 삭제 하는 요청
     public void delete(@PathVariable("id") Long number)
     {
-
         boardService.delete(number);
-
-
     }
 
 }
